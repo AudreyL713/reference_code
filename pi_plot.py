3b2597496d09052ab758baf0636b923d9cbf9e8a
AudreyL713 <2021leea@gmail.com>
2020-06-27 19:48:54 -0700

    fixing line smoothing

--- pi_plot.py
+++ pi_plot.py
@@ -119,17 +119,20 @@ class All_Graph(object):
             equation = f"y = {round(m,4)}x + {round(b,4)}"
             ax.plot(x, m*x+b, '-r', label=equation)
         elif self.best_fit==2:
-            x = np.linspace(-2,2,100)
+            x = np.linspace(x_values[0],x_values[-1],100)
             x1, m, b = np.polyfit(x_values, scans_mean, 2)
             equation = f"y = {round(x1,4)}$x^2$ + {round(m,4)}x + {round(b,4)}"
             ax.plot(x, x1*x**2 + m*x + b, '-r', label=equation)
         elif self.best_fit==3:
+            x = np.linspace(x_values[0],x_values[-1],100)
             x2, x1, m, b = np.polyfit(x_values, scans_mean, 3)
             ax.plot(x, x2*x**3 + x1*x**2 + m*x + b, '-r')
         elif self.best_fit==4:
+            x = np.linspace(x_values[0],x_values[-1],100)
             x3, x2, x1, m, b = np.polyfit(x_values, scans_mean, 4)
             ax.plot(x, x3*x**4 + x2*x**3 + x1*x**2 + m*x + b, '-r')
         elif self.best_fit==5:
+            x = np.linspace(x_values[0],x_values[-1],100)
             x4, x3, x2, x1, m, b = np.polyfit(x_values, scans_mean, 5)
             ax.plot(x, x4*x**5 + x2*x**3 + x1*x**2 + m*x + b, '-r')
 
